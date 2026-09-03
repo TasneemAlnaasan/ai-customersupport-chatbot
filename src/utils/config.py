fb509fc9e7380b14aa79ee40d215364d19c1ffc0
Tasneem Alnaasan <145112065+TasneemAlnaasan@users.noreply.github.com>
2026-09-03 19:05:30 +0000

    updating groq model

--- src/utils/config.py
+++ src/utils/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     
 
     # Virtual model for the provider
-    groq_model: str = "llama-3.3-70b-versatile"
+    groq_model: str = "openai/gpt-oss-20b"
     
 
     # Langsmith Monitoring
